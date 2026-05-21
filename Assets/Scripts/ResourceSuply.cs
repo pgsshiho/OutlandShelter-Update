c692ac06400e1fc8f3fcc3c54130aa9aa30d8f4d
CHOIMINJUN(최민준) <s24071@gsm.hs.kr>
2026-05-21 21:11:19 +0900

    supply complete
    supply complete

---
+++ Assets/Scripts/ResourceSuply.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class ResourceSuply : ResourceObject
+{
+    public override int Break(float amount)
+    {
+        int random = Random.Range(0, 3);
+        switch (random)
+        {
+            case 0:
+                BulletManager.pistolBullet += 50;
+                Notion.Log("Pistolammoget");
+                break;
+            case 1:
+                BulletManager.rifleBullet += 70;
+                Notion.Log("Rifleammoget");
+                break;
+            case 2:
+                BulletManager.shotGunBullet += 30;
+                Notion.Log("Shotgunammoget");
+                break;
+        }
+        return 0;
+    }
+}
